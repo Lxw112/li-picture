41a698103ee4e7ac17410d597ddbd8633537ddd3
lxw <1436400995@qq.com>
2025-04-08 20:34:42 +0800

    第一期，初始化项目

---
+++ src/test/java/com/lxw/lipicturebackend/LiPictureBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lxw.lipicturebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiPictureBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
